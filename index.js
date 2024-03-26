b2558077cc60299618c0cdbd3c12955d7a14e4f3
Whitney_vader <whitney@gmail.com>
2024-03-26 13:48:33 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply;
+
+multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 4) + 3;
+console.log(random);
+
+let num3 = 13;
+let num4 = 9;
+const mod = num3 % num4
+
+let max = Math.max(5, 10, 15, 20);
+console.log(max);
